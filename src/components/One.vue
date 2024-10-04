ffe825c2efa77c7f3add9f0ec938ce0cf3fb15bf
sebdevweb <spansini@squad.fr>
2024-10-04 15:56:51 +0200

    [PROJET-5-PROPS] Fetch datas.json

---
+++ src/components/One.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{ datas.title }}</h1>
+  </div>
+</template>
+
+
+<script setup>
+  const props = defineProps({
+    datas: Object
+  })
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/Two.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ datas.title_level_two }}</h2>
+  </div>
+</template>
+
+
+<script setup>
+  const props = defineProps({
+    datas: Object
+  })
+</script>
+
+
+<style scoped>
+
+</style>
